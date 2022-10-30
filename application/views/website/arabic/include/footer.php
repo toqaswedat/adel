442e2f7a02e863ece44320e250ac6f004aed770a
toqaqswedat <toqa.j.swedat@gmail.com>
2022-10-30 09:22:28 +0200

    first commit

---
+++ application/views/website/arabic/include/footer.php
@@ -0,0 +1,68 @@
+﻿ <!-- Footer Section Start -->
+<footer>
+
+  <section class="footer-Content">
+    <div class="row p-xl-1 pr-xl-5">
+      <a class="navbar-brand logo pr-xl-5" href="<?=base_url('arabic/');?>"><img  src="<?=base_url('assets/website/arabic/');?>img/logo.png" alt=""></a>
+    </div>
+    <div class="row p-xl-1 pr-xl-5">
+
+      <div class="col-4 mt-3 pr-xl-5 ">
+        <p class="pb-xl-5">بوابة قانونية متعاونة مع احدى اكبر مكاتب المحاماة <br>والاستشارات الشرعية والقانوني في جدة بالمملكة العربية <br>السعودية حتى تكون الممثل القانوني الرائد في منطقة الشرق <br> الأوسط وشمال افريقيا.</p>
+        <br>
+        <h2 class="text-warning">تطبيق بالعدل</h2>
+        <a class="navbar-brand logo" href="<?=base_url('');?>"><img  src="<?=base_url('assets/website/arabic/');?>img/appstore.svg" alt="app store"></a>
+        <a class="navbar-brand logo" href="<?=base_url('');?>"><img  src="<?=base_url('assets/website/arabic/');?>img/googleplay.svg" alt="google play"></a>
+      </div>
+
+      <div class="col-2">
+        <h4>روابط قد تهمك</h4>
+        <ul class="menu lh-1 ">
+        <li class="col pr-0"><a href="<?=base_url();?>arabic/Pages/about_us/">عن بالعدل</a></li>
+        <li class="col pr-0"><a href="<?=base_url();?>arabic/Pages/contact_us/">محامون بالعدل </a></li>
+        <li class="col pr-0"><a href="<?=base_url();?>arabic/Pages/contact_us/">التشهير السيبراني </a></li>
+        <li class="col pr-0"><a href="<?=base_url();?>arabic/Pages/contact_us/"> خطة الدفع </a></li>
+        <li class="col pr-0"><a href="<?=base_url();?>arabic/Pages/terms/">الشروط والأحكام</a></li>
+        <li class="col pr-0"><a href="<?=base_url();?>arabic/Pages/privacy_policy/">سياسة الخصوصية</a></li>
+        </ul>
+      </div>
+
+      <div class="col-2">
+        <h4>اقرأ أيضاً</h4>
+        <ul class="menu ">
+        <li class="col pr-0"><a href="<?=base_url();?>arabic/Pages/about_us/">أخبارنا </a></li>
+        <li class="col pr-0"><a href="<?=base_url();?>arabic/Pages/contact_us/">المقالات  </a></li>
+        <li class="col pr-0"><a href="<?=base_url();?>arabic/Pages/contact_us/">وثائق مفيدة </a></li>
+        <li class="col pr-0"><a href="<?=base_url();?>arabic/Pages/contact_us/"> روابط مهمة</a></li>
+        <li class="col pr-0"><a href="<?=base_url();?>arabic/Pages/terms/"> أسئلة شائعة</a></li>
+        </ul>
+      </div>
+
+      <div class="col-2">
+        <h4>اتصل بنا</h4>
+        <ul class="menu ">
+        <a  href="<?=base_url('');?>"><img class="p-xl-4" src="<?=base_url('assets/website/arabic/');?>img/phoneFooter.svg" alt="phone"></a>
+        <a href="<?=base_url('');?>"><img class="p-xl-4"  src="<?=base_url('assets/website/arabic/');?>img/chat.svg" alt="chat"></a>
+        <h4>  التواصل الاجتماعي </h4>
+        <div class="bottom-social-icons social-icon">
+          <a class="linkedin bg-white" href="https://www.linkedin.com/in/biladl-legal-467b4518b"><i class="ti-linkedin"></i></a>
+          <a class="twitter bg-white" href="https://twitter.com/MyBiladl?s=17"><i class="ti-twitter-alt"></i></a>
+          <a class="youtube bg-white" href="#"><i class="ti-youtube"></i></a>
+          <a class="facebook bg-white" href="#"><i class="ti-facebook"></i></a>
+        </div>
+        </ul>
+      </div>
+
+      
+
+      <div class="col-2">
+        <h4 class="list-inline-item  pt-0">تنويه</h4>
+        <a  href="<?=base_url('');?>"><img class="p-xl-4" src="<?=base_url('assets/website/arabic/');?>img/Vector.svg" alt="phone"></a>
+      </div>
+    </div>
+    <div class="row text-center custom-control">
+      <p>جميع الحقوق محفوظة <span class="text-warning" >@بالعدل</span> - 2022</p>
+    </div>
+  </section>
+</footer>
+</body>
---
+++ application/views/website/arabic/index2.php
@@ -0,0 +1,221 @@
+<section class="row-2 "> 
+<div class="row d-flex align-items-center ">
+  <div class="col-sm-3" >
+  <div id="myCarousel" class="carousel slide">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+      <img  src="<?=base_url('assets/website/');?>img/slider/sliderNew.jpg" onClick="window.location.href='<?=base_url('arabic/Login/member_normal_login/');?>'" />
+      </div>
+
+      <!-- <div class="item">
+      <img  src="<?=base_url('assets/website/');?>img/slider/sliderNew.jpg" onClick="window.location.href='<?=base_url('arabic/Login/member_normal_login/');?>'" />
+      </div>
+    
+      <div class="item">
+      <img  src="<?=base_url('assets/website/');?>img/slider/sliderNew.jpg" onClick="window.location.href='<?=base_url('arabic/Login/member_normal_login/');?>'" />
+      </div> -->
+    </div>
+  </div>
+  </div>
+
+
+  <div class="col-sm-1">  
+    <div class="row-1">
+      <div class="icon ">
+      <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/chatwithus.svg" />
+      </div>
+      <h6>اتصل بنا</h6><br><br>
+      </a>
+    </div>
+    <div class="row-1">
+      <div class="icon">
+      <img class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/chat-with-us.svg" />
+      </div>
+      <h6>تحدث معنا</h6><br><br>
+      </a>
+    </div>
+    <div class="row-1">
+      <div class="icon">
+      <img class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/advice.svg" />
+      </div>
+      <h6>نصيحة قانونية</h6><br><br>
+      </a>
+    </div>
+  </div>
+
+
+  <div class="col-md-6 col-sm-12 col-xs-12 home-section-top ">
+    <div>
+      <h1 class="row-5">احصل معنا علي نصيحة قانونية</h1>
+      <p class="row-2 mt-3">بالعدل هنا لتساعدك في الحصول علي استشارة قانونية موثوقة لحل أي  <br>مشكلة أو موقف قانوني مثل مشاكل في العمل, الأحوال الشخصية, العمل <br>ولمشاكل التجارية مثل الايجارات أو أي موقف قانوني آخر يمكن للمحامي حله.</p>
+      <button class="bg-dark font-weight-bold text-light datepicker w-25 mt-5">انضم الآن</button>
+    </div>
+  </div>
+
+</div>
+</section>
+
+
+<section class="row-3">
+  <h1 class="text-center custom-control"><u class="mt-2">قد ي</u>همك أيضاً </h1>
+<div class="row d-flex align-items-center mt-5 ">
+  <div class="col-sm-4 d-flex flex-column justify-content-center align-items-end" >
+    <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/lawers.svg" />
+    <h6 class="mt-5">محامون بالعدل</h6>
+  </div>
+  <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center" >
+    <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/crimes.svg" />
+    <h6 class="mt-5">التشهير السيبراني</h6>
+  </div>
+  <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center" >
+    <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/links.svg" />
+    <h6 class="mt-5">روابط مهمة</h6>
+  </div>
+  <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center" >
+    <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/files.svg" />
+    <h6 class="mt-5">وثائق مفيدة</h6>
+  </div>
+  <div class="col-sm-4 d-flex flex-column justify-content-center align-items-start" >
+    <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/questions.svg" />
+    <h6 class="mt-5">الأسئلة الشائعة</h6>
+  </div>
+  </div>
+</section>
+
+<section class="row-3">
+  <div class="col-md-6 col-sm-12 col-xs-12  pr-xl-5">
+      <div class="pr-xl-5">
+        <h1 class="row-5">عن بالعدل</h1>
+        <p class="row-2 mt-3">
+        بالعدل هي بوابة قانونية متعاونة مع احدى اكبر مكاتب المحاماة والاستشارات الشرعية <br>والقانوني في جدة بالمملكة العربية السعودية حتى تكون الممثل القانوني الرائد في <br>منطقة الشرق الأوسط وشمال افريقيا. يتكون الممثل القانوني ل(بالعدل) من فريق ذو<br> كفاءة عالية مختارين بعناية ولديهم خلفيات علمية متنوعة حتى يتمكن بالعدل من اعطاء <br>المشورة القانوينة بناء على مستوى عالي من الخبرة. 
+        </p>
+        <button class="bg-dark font-weight-bold text-light datepicker w-25 mt-5"> معرفة المزيد</button>
+      </div>
+  </div>
+  <div class="">
+    <img  class='' src="<?=base_url('assets/website/arabic/');?>img/Rectangle.svg" />
+  </div>
+</section>
+
+
+
+
+
+
+
+<section class="row-2 "> 
+<div class="row d-flex align-items-center ">
+  <div class="col-sm-3" >
+  <div id="myCarousel" class="carousel slide">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+      <img  src="<?=base_url('assets/website/');?>img/slider/sliderNew.jpg" onClick="window.location.href='<?=base_url('arabic/Login/member_normal_login/');?>'" />
+      </div>
+
+      <!-- <div class="item">
+      <img  src="<?=base_url('assets/website/');?>img/slider/sliderNew.jpg" onClick="window.location.href='<?=base_url('arabic/Login/member_normal_login/');?>'" />
+      </div>
+    
+      <div class="item">
+      <img  src="<?=base_url('assets/website/');?>img/slider/sliderNew.jpg" onClick="window.location.href='<?=base_url('arabic/Login/member_normal_login/');?>'" />
+      </div> -->
+    </div>
+  </div>
+  </div>
+
+
+  <div class="col-sm-1">  
+    <div class="row-1">
+      <div class="icon ">
+      <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/chatwithus.svg" />
+      </div>
+      <h6>اتصل بنا</h6><br><br>
+      </a>
+    </div>
+    <div class="row-1">
+      <div class="icon">
+      <img class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/chat-with-us.svg" />
+      </div>
+      <h6>تحدث معنا</h6><br><br>
+      </a>
+    </div>
+    <div class="row-1">
+      <div class="icon">
+      <img class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/advice.svg" />
+      </div>
+      <h6>نصيحة قانونية</h6><br><br>
+      </a>
+    </div>
+  </div>
+
+
+  <div class="col-md-6 col-sm-12 col-xs-12 home-section-top ">
+    <div>
+      <h1 class="row-5">احصل معنا علي نصيحة قانونية</h1>
+      <p class="row-2 mt-3">بالعدل هنا لتساعدك في الحصول علي استشارة قانونية موثوقة لحل أي  <br>مشكلة أو موقف قانوني مثل مشاكل في العمل, الأحوال الشخصية, العمل <br>ولمشاكل التجارية مثل الايجارات أو أي موقف قانوني آخر يمكن للمحامي حله.</p>
+      <button class="bg-dark font-weight-bold text-light datepicker w-25 mt-5">انضم الآن</button>
+    </div>
+  </div>
+
+</div>
+</section>
+
+
+<section class="row-3">
+  <h1 class="text-center custom-control"><u class="mt-2">قد ي</u>همك أيضاً </h1>
+<div class="row d-flex align-items-center mt-5 ">
+  <div class="col-sm-4 d-flex flex-column justify-content-center align-items-end" >
+    <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/lawers.svg" />
+    <h6 class="mt-5">محامون بالعدل</h6>
+  </div>
+  <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center" >
+    <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/crimes.svg" />
+    <h6 class="mt-5">التشهير السيبراني</h6>
+  </div>
+  <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center" >
+    <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/links.svg" />
+    <h6 class="mt-5">روابط مهمة</h6>
+  </div>
+  <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center" >
+    <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/files.svg" />
+    <h6 class="mt-5">وثائق مفيدة</h6>
+  </div>
+  <div class="col-sm-4 d-flex flex-column justify-content-center align-items-start" >
+    <img  class='img_home_section' src="<?=base_url('assets/website/arabic/');?>img/icons/questions.svg" />
+    <h6 class="mt-5">الأسئلة الشائعة</h6>
+  </div>
+  </div>
+</section>
+
+<section class="row-3">
+  <div class="col-md-6 col-sm-12 col-xs-12  pr-xl-5">
+      <div class="pr-xl-5">
+        <h1 class="row-5">عن بالعدل</h1>
+        <p class="row-2 mt-3">
+        بالعدل هي بوابة قانونية متعاونة مع احدى اكبر مكاتب المحاماة والاستشارات الشرعية <br>والقانوني في جدة بالمملكة العربية السعودية حتى تكون الممثل القانوني الرائد في <br>منطقة الشرق الأوسط وشمال افريقيا. يتكون الممثل القانوني ل(بالعدل) من فريق ذو<br> كفاءة عالية مختارين بعناية ولديهم خلفيات علمية متنوعة حتى يتمكن بالعدل من اعطاء <br>المشورة القانوينة بناء على مستوى عالي من الخبرة. 
+        </p>
+        <button class="bg-dark font-weight-bold text-light datepicker w-25 mt-5"> معرفة المزيد</button>
+      </div>
+  </div>
+  <div class="">
+    <img  class='' src="<?=base_url('assets/website/arabic/');?>img/Rectangle.svg" />
+  </div>
+</section>
+
+
+
+
